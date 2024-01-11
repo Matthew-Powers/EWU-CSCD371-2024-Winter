388e19811b59b84f162b502a0c3490e0a7477f3a
M P <mpowers16@ewu.edu>
2024-01-11 13:10:56 -0800

    FIxed issue where questions were not being added when being loaded in

--- PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
+++ PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
@@ -88,6 +88,7 @@ public class Program
             question.Answers[1] = answer2;
             question.Answers[2] = answer3;
             question.CorrectAnswerIndex = correctAnswerIndex;
+            questions[i] = question;
         }
         return questions;
     }
