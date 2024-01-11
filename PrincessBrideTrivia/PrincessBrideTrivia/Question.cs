bc1e8a07128ef4f152a9fc1ee19bf3ed35e2c4d2
M P <mpowers16@ewu.edu>
2024-01-11 13:00:18 -0800

    Fixed the unit test, still need to get the program to not crash

---
+++ PrincessBrideTrivia/PrincessBrideTrivia/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace PrincessBrideTrivia;
+
+public class Question
+{
+    public string Text { get; set; }
+    public string[] Answers { get; set; }
+    public string CorrectAnswerIndex { get; set; }
+}
